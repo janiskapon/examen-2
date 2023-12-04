96ff5bc2e046a0fbbe6009a6086b3303a318d70c
janiskapon <Janis.Kapon@studentfr.ch>
2023-12-04 16:55:18 +0100

    Correction et creation du B.Menu
    Création du Burger Menu, beugs dans la navigation. A completer mercredi.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,8 @@
+var burger = document.querySelector(".burger");
+var menu = document.querySelector(".menu");
+
 var toggleMenu = function toggleMenu() {
   menu.classList.toggle("is-active");
 };
-
-var button = document.querySelector(".button");
-var menu = document.querySelector(".menu");
-var openMenu = function () {
-  menu.classList.toggle("is-open");
-  button.classList.toggle("is-active");
-};
-
-button.addEventListener("click", openMenu);
 
 burger.addEventListener("click", toggleMenu);
-
-var toggleMenu = function toggleMenu() {
-  menu.classList.toggle("is-active");
-  burger.classList.toggle("is-active");
-};
-
-
