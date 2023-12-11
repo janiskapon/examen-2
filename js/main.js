c290b31f4931573bbc15168ff2ee2a86718f7bee
janiskapon <Janis.Kapon@studentfr.ch>
2023-12-11 15:05:31 +0100

    correction burger menu, il fonctionne enfin

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ var menu = document.querySelector(".menu");
 
 var toggleMenu = function toggleMenu() {
   menu.classList.toggle("is-active");
+  burger.classList.toggle("is-active");
 };
 
 burger.addEventListener("click", toggleMenu);
+menu.addEventListener("click", toggleMenu);
